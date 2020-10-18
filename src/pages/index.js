eddb0346ab386cca312a7f26bfd8cffac738d947
Aquiles Lacruz <aquiles.lacruz@gmail.com>
2020-10-17 23:01:10 -0300

    Changed the homepage a bit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,25 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import styles from "../styles/index.module.scss"
+
+const Option = ({link='/', name='', img=null}) => (
+  <Link to={link} className={styles.option}>
+      {name}
+  </Link>
+)
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="options">
-      <Link to="/capitols">Capitols</Link>
+    <div className={styles.intro}>
+      {`This is just a simple app where we'll be adding some Geographical trivia games and practice React.`}
+      <br />
+      <br />
+      {`We hope you enjoy it.`}
+    </div>
+    <div className={styles.options}>
+      <Option link='/capitols' name='Capitols' key='capitols' />
     </div>
   </Layout>
 )
